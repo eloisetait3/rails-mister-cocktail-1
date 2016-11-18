ebf52efd00e1ac7a1b485592309d47e8ade59533
Andy B <andybarnov@gmail.com>
2016-11-18 16:26:43 +0100

    create helper to pick the stub if an image is absent

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def image_or_stub(model, stub)
+    if model.photo
+      cl_image_path model.photo.path, height: 500, width: 500, crop: :crop
+    else
+      cl_image_path stub, height: 500, width: 500, crop: :fit
+    end
+  end
 end
